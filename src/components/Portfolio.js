ff173e2cddc03f3afedb771e3f429b674bd73e29
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-11-10 12:40:18 -0600

    importing pages to portfolio component

---
+++ src/components/Portfolio.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import Nav from './Nav';
+import Home from './pages/Home'
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import Footer from './pages/Footer';
+import "../css/styles.css"
