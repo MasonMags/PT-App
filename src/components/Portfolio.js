9fb166c3de4602608ed2b03cf4dc9bf9346362c3
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-11-10 12:47:42 -0600

    handling page changes with state

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,3 +6,31 @@ import Services from './pages/Services';
 import Contact from './pages/Contact';
 import Footer from './pages/Footer';
 import "../css/styles.css"
+
+export default function Portfolio() {
+    const [currentPage, setCurrentPage] = useState('Home');
+
+    const renderPage = () => {
+        if (currentPage === 'Home'){
+            return <Home />
+        };
+        if (currentPage === 'About'){
+            return <About />
+        };
+        if (currentPage === 'Services'){
+            return <Services />
+        };
+        if (currentPage === 'Contact')
+            return <Contact />
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+          <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+          {renderPage()}
+          <Footer />
+        </div>
+      );
+};
