2dee66c3073809ef6f36c7193b9780bc001f76b0
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-11-10 14:32:20 -0600

    added nav tabs and functionality

---
+++ src/components/Nav.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+function Nav({ currentPage, handlePageChange }){
+    return(
+      <ul className="nav nav-tabs nav-overwrite">
+      <li className="nav-item">
+        <a
+          href="#home"
+          onClick={() => handlePageChange('Home')}
+          //  TODO: Add a comment explaining what this logic is doing
+
+          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+        >
+          Home
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#about"
+          //  TODO: Add a comment explaining what this logic is doing
+
+          onClick={() => handlePageChange('About')}
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          //  TODO: Add a comment explaining what this logic is doing
+
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#services"
+          //  TODO: Add a comment explaining what this logic is doing
+
+          onClick={() => handlePageChange('Services')}
+          className={currentPage === 'Services' ? 'nav-link active' : 'nav-link'}
+        >
+          Services
+        </a>
+      </li>
+    </ul>
+    );
+};
+
+export default Nav
