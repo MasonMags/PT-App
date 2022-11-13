b73ceb7d23cd62f8614ca925ab35b3773d98f3e4
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-11-13 13:26:07 -0600

    added footer

---
+++ src/components/pages/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// import githubIcon from '../../images/github-small.png'
+// import linkedinIcon from '../../images/linkedin-icon.png'
+
+/* eslint-disable */
+export default function Footer() {   
+    return (
+    <footer className="footer fixed-bottom">
+        <div>    
+            <p>
+                footer
+            </p>
+        </div>
+    </footer>
+    )
+}
