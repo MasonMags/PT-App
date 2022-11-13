30c032eec0034a3d0f52c4bda8c62f8cfa6667b8
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-11-13 13:27:19 -0600

    added home and services page

---
+++ src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// import profilepic from '../../images/profilepic.jpg'
+
+export default function Home() {
+    return (
+        <div className="home-container">
+                <div className="home-content">
+                 {/* <img className="avatar" src={profilepic} alt="Mason on the river"></img> */}
+                    <p>
+                      bing bong here is my home page
+                    </p>
+                </div>
+        </div>
+    );
+};
---
+++ src/components/pages/Services.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Services() {
+    return (
+        <div className="home-container">
+                <div className="home-content">
+                 {/* <img className="avatar" src={profilepic} alt="Mason on the river"></img> */}
+                    <p>
+                    bing bong here are my Services
+                    </p>
+                </div>
+        </div>
+    );
+}
